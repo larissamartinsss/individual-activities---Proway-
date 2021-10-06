859a7b4f48c6f7a509c81c60d588f525fbcd6bdc
Larissa Martins <larissamartins.job@gmail.com>
2021-10-06 11:45:23 -0400

    Classe conta, getters and setters criados

---
+++ src/Atividade021/src/Conta.java
@@ -0,0 +1,20 @@
+public class Conta {
+    private double saldo;
+    private int codCliente;
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getCodCliente() {
+        return this.codCliente;
+    }
+
+    public void setCodCliente(int codCliente) {
+        this.codCliente = codCliente;
+    }
+}
