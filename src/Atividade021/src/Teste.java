b1e858718b7d024f84831522037e9ef7e1aca00f
Larissa Martins <larissamartins.job@gmail.com>
2021-10-06 12:00:20 -0400

    Classe teste criada, setters atribuidos.

---
+++ src/Atividade021/src/Teste.java
@@ -0,0 +1,16 @@
+public class Teste {
+    public static void main(String[] args) {
+        // objeto conta:
+        Conta conta = new Conta();
+        // objeto conta-corrente:
+        ContaCorrente cc = new ContaCorrente();
+        // set conta:
+        conta.setSaldo(23.750);
+        conta.setCodCliente(21851);
+        //set conta corrente:
+        cc.setSaldo(11.500);
+        cc.setCodCliente(321251);
+
+
+    }
+}
