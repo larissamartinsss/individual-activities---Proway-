2cb2b6e3c43a9ec53f67d8a6a43b652826c46b74
Larissa Martins <larissamartins.job@gmail.com>
2021-10-07 15:16:21 -0400

    Criando os metodos da conta

--- src/Atividade022/src/Banco/ContaCorrente.java
+++ src/Atividade022/src/Banco/ContaCorrente.java
@@ -2,11 +2,21 @@ package Banco;
 
 public class ContaCorrente {
     private double saldo;
-    private double taxaSaque;
+    private double taxaSaque = 2.50;
     private int codCliente;
 
     public ContaCorrente(double saldo, int codCliente) {
         this.saldo = saldo;
         this.codCliente = codCliente;
     }
+
+    public void saque(double valor){
+        double saque = (this.saldo - valor - this.taxaSaque);
+    }
+
+    public void deposito(double valor){
+        double saldoAtual = this.saldo + valor;
+    }
+
 }
+
