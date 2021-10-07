06e60597194c963b7299b64ef11bced331ba8177
Larissa Martins <larissamartins.job@gmail.com>
2021-10-07 14:56:46 -0400

    Iniciando atividade 22

---
+++ src/Atividade022/src/Aluno.java
@@ -0,0 +1,30 @@
+public class Aluno {
+    private String nomeCompleto;
+    private int idade;
+    private int matricula;
+
+
+    public String getNomeCompleto() {
+        return this.nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getMatricula() {
+        return this.matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+}
