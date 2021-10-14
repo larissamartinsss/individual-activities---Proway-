5e4c88fcb4f87cdac0b69ae6f2d0bf0909437d04
Larissa Martins <larissamartins.job@gmail.com>
2021-10-14 15:47:18 -0400

    adicionando switch case

---
+++ src/Atividade023/src/Controller/Controller.java
@@ -0,0 +1,21 @@
+package Controller;
+
+public class Controller {
+
+    public void salvar(){
+
+    }
+
+    public void ler(){
+
+    }
+
+    public void alterar(){
+
+    }
+
+    public void remover(){
+
+    }
+
+}
---
+++ src/Atividade023/src/View/Main.java
@@ -0,0 +1,22 @@
+package View;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a opção desejada: ");
+        int opcao = Integer.parseInt(sc.nextLine());
+
+        switch (opcao){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+    }
+}
