87d9518bc69fb82f1c4ac120437966ddc1dc1fb8
Larissa Martins <larissamartins.job@gmail.com>
2021-10-14 15:39:22 -0400

    Iniciando atividade 23.

---
+++ src/Atividade023/src/Model/CadLivros.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class CadLivros {
+    private int id;
+    private String titulo;
+    private String Autor;
+    private String genero;
+    private int numPags;
+
+    public CadLivros(int id) {
+        this.id = id;
+    }
+}
