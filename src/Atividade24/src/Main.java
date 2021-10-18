9511fce358e5d5aff9a9537dec1243491db39816
Larissa Martins <larissamartins.job@gmail.com>
2021-10-18 14:20:02 -0400

    atividade 24 concluída!

--- src/Atividade24/src/Main.java
+++ src/Atividade24/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         // carro:
         carro.marca = "Mercedes-benz";
         carro.modelo = "Glb 200 2021";
+        carro.placa = "CND2131";
         carro.porta_malas = 3.80;
         carro.portas = 4;
 
@@ -20,8 +21,8 @@ public class Main {
         System.out.printf("Marca: %s \nModelo: %s\nPlaca: %s\n ", veicObj.marca, veicObj.modelo, veicObj.placa);
 
         Carro carObj =(Carro) obj2;
-        System.out.println("-----------------------------------------");
-        System.out.printf("Marca: %s \nModelo: %s\nPlaca: %s\nNº de portas: %d\nCapacidade porta-malas: %d\n"
+        System.out.println("----------------------------------------");
+        System.out.printf("Marca: %s \nModelo: %s\nPlaca: %s\nNº de portas: %d\nCapacidade porta-malas: %.2f\n"
                 , carObj.marca, carObj.modelo, carObj.placa, carObj.portas, carObj.porta_malas);
 
 
