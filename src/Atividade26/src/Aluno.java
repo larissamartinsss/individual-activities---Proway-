62e3b41a29ee8de3de06ae568e2ad91ebfa4f557
Larissa Martins <larissamartins.job@gmail.com>
2021-10-19 13:55:11 -0400

    Iniciando atividade prática 26

---
+++ src/Atividade26/src/Aluno.java
@@ -0,0 +1,20 @@
+import java.util.Objects;
+
+public class Aluno extends Pessoa {
+    public int matricula;
+    public String turma;
+    public String curso;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Aluno)) return false;
+        Aluno aluno = (Aluno) o;
+        return matricula == aluno.matricula && Objects.equals(turma, aluno.turma) && Objects.equals(curso, aluno.curso);
+    }
+
+
+
+
+
+}
---
+++ src/Atividade26/src/Pessoa.java
@@ -0,0 +1,16 @@
+import java.util.Objects;
+
+public class Pessoa {
+    public String nome;
+    public String sobrenome;
+    public int idade;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pessoa)) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return idade == pessoa.idade && Objects.equals(nome, pessoa.nome) && Objects.equals(sobrenome, pessoa.sobrenome);
+    }
+
+}
