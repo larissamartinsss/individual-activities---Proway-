2d5d2257049a043b1f16f5ebd1f8ab1fbac75454
Larissa Martins <larissamartins.job@gmail.com>
2021-10-20 16:12:54 -0400

    tá funcionando! pq? scr

--- src/Atividade27/src/view/Main.java
+++ src/Atividade27/src/view/Main.java
@@ -6,6 +6,7 @@ import model.Cadastro;
 
 public class Main {
     public static void main(String[] args) {
+        Filmes filmes = new Filmes();
         Filmes f = new Filmes();
         f.id = 1;
         f.titulo = "Capitão Fantástico";
@@ -13,12 +14,21 @@ public class Main {
         f.ano = 2016;
         f.duracao = "1h58m";
 
+        Series series = new Series();
         Series s = new Series();
-        s.id = 1;
+        s.id = 2;
         s.titulo = "Anne with an E";
         s.genero = " Coming of age";
         s.ano = 2017;
         s.duracao = "3 temporadas";
+
+        filmes.create(f);
+        series.create(s);
+
+        System.out.println(f);
+        System.out.println(s);
+
+
 
 
     }
