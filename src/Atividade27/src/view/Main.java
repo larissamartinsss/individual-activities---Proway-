b0faa49d4a0ac3b3f073403736698b656f406dfb
Larissa Martins <larissamartins.job@gmail.com>
2021-10-20 16:22:29 -0400

    finalizado?

--- src/Atividade27/src/view/Main.java
+++ src/Atividade27/src/view/Main.java
@@ -20,15 +20,27 @@ public class Main {
         s.genero = " Coming of age";
         s.ano = 2017;
         s.duracao = "3 temporadas";
+        Series s2 = new Series();
+        s2.id = 3;
+        s2.titulo = "The Bold Type";
+        s2.genero = " Comédia dramatica";
+        s2.ano = 2017;
+        s2.duracao = "5 temporadas";
+
+        //s = s2;
 
         filmes.create(f);
         series.create(s);
+        series.delete(s);
+        series.create(s2);
 
         System.out.println(f);
         System.out.println(s);
+        System.out.println(s2);
 
         System.out.println(filmes.exist(f));
         System.out.println(series.exist(s));
+        System.out.println(series.exist(s2));
 
     }
 }
