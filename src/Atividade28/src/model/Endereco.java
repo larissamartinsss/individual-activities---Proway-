a7aff478c541c46d6b49f0175004ee5f0cf8ebc1
Larissa Martins <larissamartins.job@gmail.com>
2021-10-21 13:55:09 -0400

    Iniciando atividade 28

---
+++ src/Atividade28/src/model/Endereco.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Endereco {
+    public String Lougradoro;
+    public String numero;
+    public String cep;
+    public String bairro;
+    public String cidade;
+
+}
