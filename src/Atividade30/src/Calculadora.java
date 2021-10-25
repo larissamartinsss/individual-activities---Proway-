43edcfe45b3fdb048e26d4e0676b4882d5ae33d5
Larissa Martins <larissamartins.job@gmail.com>
2021-10-25 13:19:36 -0400

    bugs & more bugs for goodness sake, help

--- src/Atividade30/src/Calculadora.java
+++ src/Atividade30/src/Calculadora.java
@@ -5,29 +5,35 @@ public class Calculadora {
     //_  O sistema deve possuir um tratamento de exceção durante a leitura dos dados para evitar que o usuário
     // insira algo diferente de números inteiros tanto para as operação quanto para o menu.
 
-        public int soma(int n1, int n2){
-            int soma = n1 + n2;
-            return soma;
-        }
+    public int soma(int n1, int n2) {
+        int soma = n1 + n2;
+        return soma;
+    }
 
-        public int substracao(int n1, int n2){
-            int subtracao = n1 - n2;
-            return subtracao;
-        }
+    public int substracao(int n1, int n2) {
+        int subtracao = n1 - n2;
+        return subtracao;
+    }
 
-        public int multiplicacao(int n1, int n2){
-            int multiplicacao = n1 - n2;
-            return multiplicacao;
-         }
+    public int multiplicacao(int n1, int n2) {
+        int multiplicacao = n1 * n2;
+        return multiplicacao;
+    }
 
-        public int divisao(int n1, int n2){
-            int divisao = n1 - n2;
+    public int divisao(int n1, int n2) {
+        int divisao = n1 / n2;
+        if (n2 != 0) {
             return divisao;
+        } else {
+            throw new DivisaoPorZeroException("Erro: Divisão por zero");
         }
+    }
 
-
-
-
+    public class DivisaoPorZeroException extends RuntimeException {
+        public DivisaoPorZeroException(String message) {
+            super(message);
+        }
+    }
 
 
 }
--- src/Atividade30/src/Main.java
+++ src/Atividade30/src/Main.java
@@ -4,41 +4,53 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculadora calc = new Calculadora();
+        int n1, n2;
+        int opcao = -1;
 
-            System.out.println("\nEscolha a opção desejada: ");
-            int opcao = Integer.parseInt(sc.nextLine());
-
-            switch (opcao){
+        System.out.println("\nEscolha a opção desejada: ");
+        System.out.println("\n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão");
+        opcao = Integer.parseInt(sc.nextLine());
+       // do {
+            switch (opcao) {
                 case 1:
                     System.out.println("\nDigite os números a serem somados: ");
-                    int n1 = Integer.parseInt(sc.nextLine()); int n2 = Integer.parseInt(sc.nextLine());
+                     n1 = Integer.parseInt(sc.nextLine());
+                     n2 = Integer.parseInt(sc.nextLine());
                     System.out.println(calc.soma(n1, n2));
 
                     break;
 
                 case 2:
                     System.out.println("\nDigite os números a serem subtraídos: ");
-                    n1 = Integer.parseInt(sc.nextLine()); n2 = Integer.parseInt(sc.nextLine());
+                    n1 = Integer.parseInt(sc.nextLine());
+                    n2 = Integer.parseInt(sc.nextLine());
                     System.out.println(calc.substracao(n1, n2));
 
                     break;
 
                 case 3:
                     System.out.println("\nDigite os números a serem multiplicados: ");
-                    n1 = Integer.parseInt(sc.nextLine()); n2 = Integer.parseInt(sc.nextLine());
+                    n1 = Integer.parseInt(sc.nextLine());
+                    n2 = Integer.parseInt(sc.nextLine());
                     System.out.println(calc.multiplicacao(n1, n2));
 
                     break;
 
                 case 4:
                     System.out.println("\nDigite os números a serem divididos: ");
-                    n1 = Integer.parseInt(sc.nextLine()); n2 = Integer.parseInt(sc.nextLine());
-                    System.out.println(calc.divisao(n1, n2));
+                    try{
+                        n1 = Integer.parseInt(sc.nextLine());
+                        n2 = Integer.parseInt(sc.nextLine());
+                        System.out.println(calc.divisao(n1, n2));
+                    } catch(ArithmeticException ex){
+                        System.out.println("Não é possível realizar divisao por zero");
+                    }
                     break;
 
                 case 0:
                     System.out.println("Saindo...");
                     break;
             }
-        }
+       // } while (opcao > 0 && opcao < 5);
     }
+}
