b699dafaa7ec3a363fffc02b4a0c874c7877a7ef
Larissa Martins <larissamartins.job@gmail.com>
2021-10-25 14:02:01 -0400

    tá funcionando.....

--- src/Atividade30/src/Main.java
+++ src/Atividade30/src/Main.java
@@ -4,45 +4,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculadora calc = new Calculadora();
-        int n1, n2;
+        int n1 = 0;
+        int n2 = 0;
         int opcao = -1;
 
-        System.out.println("\nEscolha a opção desejada: ");
-        System.out.println("\n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão");
-        opcao = Integer.parseInt(sc.nextLine());
-       // do {
+        do {
+            System.out.println("\n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão\n");
+            opcao = lerNum(sc, "\nEscolha a opção desejada:");
+             if (opcao > 0 && opcao < 5) {
+                n1 = lerNum(sc, "Diite o 1º número: ");
+                n2 = lerNum(sc, "Diite o 2º número: ");
+            }
+
             switch (opcao) {
                 case 1:
-                    System.out.println("\nDigite os números a serem somados: ");
-                     n1 = Integer.parseInt(sc.nextLine());
-                     n2 = Integer.parseInt(sc.nextLine());
-                    System.out.println(calc.soma(n1, n2));
-
+                    System.out.println("O resultado da soma é: "+calc.soma(n1, n2));
                     break;
 
                 case 2:
-                    System.out.println("\nDigite os números a serem subtraídos: ");
-                    n1 = Integer.parseInt(sc.nextLine());
-                    n2 = Integer.parseInt(sc.nextLine());
-                    System.out.println(calc.substracao(n1, n2));
+                    System.out.println("O resultado da subtração é: "+calc.substracao(n1,n2));
 
                     break;
 
                 case 3:
-                    System.out.println("\nDigite os números a serem multiplicados: ");
-                    n1 = Integer.parseInt(sc.nextLine());
-                    n2 = Integer.parseInt(sc.nextLine());
-                    System.out.println(calc.multiplicacao(n1, n2));
+                    System.out.println("O resultado da multiplicação é: "+calc.multiplicacao(n1, n2));
 
                     break;
 
                 case 4:
                     System.out.println("\nDigite os números a serem divididos: ");
-                    try{
-                        n1 = Integer.parseInt(sc.nextLine());
-                        n2 = Integer.parseInt(sc.nextLine());
-                        System.out.println(calc.divisao(n1, n2));
-                    } catch(ArithmeticException ex){
+                    try {
+                        System.out.println("O resultado da divisão é: "+calc.divisao(n1, n2));
+                    } catch (ArithmeticException ex) {
                         System.out.println("Não é possível realizar divisao por zero");
                     }
                     break;
@@ -51,6 +44,23 @@ public class Main {
                     System.out.println("Saindo...");
                     break;
             }
-       // } while (opcao > 0 && opcao < 5);
+
+        } while (opcao != 0);
+
+    }
+    public static int lerNum(Scanner sc, String mensagem){
+        int num = 0;
+        boolean valido = false;
+        do{
+            try{
+                System.out.print(mensagem);
+                num = Integer.parseInt(sc.nextLine());
+                valido = true;
+            } catch(NumberFormatException ex){
+                System.out.println("Valor digitado é inválido!");
+            }
+        } while(!valido);
+
+        return num;
     }
 }
