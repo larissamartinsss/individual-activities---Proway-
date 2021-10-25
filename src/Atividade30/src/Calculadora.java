ecb18d7db80a49160741dfa224a8218c4401eca2
Larissa Martins <larissamartins.job@gmail.com>
2021-10-25 09:18:49 -0400

    Iniciando atividade 30.

---
+++ src/Atividade30/src/Calculadora.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    // _  Crie uma calculadora para realizar as quatro operações básicas: soma, subtração, multiplicação e divisão.
+    //_  Deve possuir um menu para o usuário escolher a operação a realizar.
+    //_  O sistema deve possuir um tratamento de exceção para evitar erro em uma divisão por zero.
+    //_  O sistema deve possuir um tratamento de exceção durante a leitura dos dados para evitar que o usuário
+    // insira algo diferente de números inteiros tanto para as operação quanto para o menu.
+
+        public void soma(double n1, double n2){
+            double soma = n1 + n2;
+        }
+
+        public void substracao(double n1, double n2){
+            double subtracao = n1 - n2;
+        }
+
+        public void multiplicacao(double n1, double n2){
+            double multiplicacao = n1 - n2;
+         }
+
+        public void divisao(double n1, double n2){
+        double divisao = n1 - n2;
+        }
+
+        public void menu(){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Escolha a opção desejada: ");
+            int opcao = Integer.parseInt(sc.nextLine());
+
+
+
+        }
+
+
+
+
+}
